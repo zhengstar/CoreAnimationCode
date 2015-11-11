90204beb075770b217e23a50925ce86f9e427c2c
zhengmingxing <zhengmingxing@rong360.com>
2015-11-11 15:24:49 +0800

    增加第四章

--- chapter 3/chapter 3/ViewController.m
+++ chapter 3/chapter 3/ViewController.m
@@ -7,21 +7,79 @@
 //
 
 #import "ViewController.h"
-
+#import <QuartzCore/QuartzCore.h>
 @interface ViewController ()
-
+@property (nonatomic, strong) UIView *layerView;
+@property (nonatomic, strong) CALayer *blueLayer;
 @end
 
 @implementation ViewController
-
+@synthesize blueLayer;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.layerView = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 300, 300)];
+    self.layerView.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:self.layerView];
+    
+    blueLayer = [CALayer layer];
+    blueLayer.frame = CGRectMake(50.0f, 50.0f, 100.0f, 100.0f);
+    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    [self.layerView.layer addSublayer:blueLayer];
+    
+    //[self.layerView.layer display];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint point = [[touches anyObject] locationInView:self.view];
+    point = [self.layerView.layer convertPoint:point fromLayer:self.view.layer];
+//    if ([self.layerView.layer containsPoint:point]) {
+//        point = [self.blueLayer convertPoint:point fromLayer:self.layerView.layer];
+//        if ([self.blueLayer containsPoint:point]) {
+//            [[[UIAlertView alloc] initWithTitle:@"Inside Blue Layer"
+//                                        message:nil
+//                                       delegate:nil
+//                              cancelButtonTitle:@"OK"
+//                              otherButtonTitles:nil] show];
+//        }
+//        else {
+//            [[[UIAlertView alloc] initWithTitle:@"Inside White Layer"
+//                                        message:nil
+//                                       delegate:nil
+//                              cancelButtonTitle:@"OK"
+//                              otherButtonTitles:nil] show];
+//        }
+//    }
+//    else {
+//        
+//    }
+    CALayer *layer = [self.layerView.layer hitTest:point];
+    if (layer == self.blueLayer) {
+        [[[UIAlertView alloc] initWithTitle:@"Inside Blue Layer"
+                                    message:nil
+                                   delegate:nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil] show];
+    }
+    else if (layer == self.layerView.layer) {
+        [[[UIAlertView alloc] initWithTitle:@"Inside White Layer"
+                                    message:nil
+                                   delegate:nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil] show];
+    }
+    else {
+        [[[UIAlertView alloc] initWithTitle:@"Outside White Layer"
+                                    message:nil
+                                   delegate:nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil] show];
+    }
 }
 
 @end
