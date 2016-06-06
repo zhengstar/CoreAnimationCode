9e6d66482dfa8260d002c1f2b296d3eddf1d5880
zhengmingxing <zhengmingxing@rong360.com>
2016-06-06 15:45:39 +0800

    颜色渐变

---
+++ chapter6_5/chapter6_5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  chapter6_5
+//
+//  Created by zmx on 16/6/6.
+//  Copyright © 2016年 zmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
